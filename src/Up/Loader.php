a4707e2b130a501db2daafd267096cae4d6f3f8c
Rqtes Dev <89046940+Rqtes-Dev@users.noreply.github.com>
2021-08-21 12:39:34 -0500

    Create Loader.php

---
+++ src/Up/Loader.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Up;
+
+use pocketmine\Player;
+use pocketmine\math\Vector3;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+
+class Loader extends PluginBase {
+  
+  public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args): bool {
+    if(strtolower($cmd->getName()) === "up"){
+      if(empty($args[0])){
+        $sender->sendMessage("§cUsage: /up <height>");
+        return true;
+      }
+      $x = $sender->getX();
+      $y = $sender->getY();
+      $z = $sender->getZ();
+      $sender->teleport(new Vector3($x, $y + $args[0], $z));
+      $sender->sendMessage("you raised §b$args[0] §7blocks");
+    }
+  }
+  
+}
